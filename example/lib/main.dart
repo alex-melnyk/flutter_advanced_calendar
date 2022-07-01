4fc29cb29081ed9437b8162b297fbbb64c35f44c
Oleksandr Zaliskyi <oleksandr.zaliskyi@litslink.com>
2022-07-01 16:43:38 +0300

    update-lints-version: Fix lint warnings

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_advanced_calendar/flutter_advanced_calendar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
